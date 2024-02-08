b864b5455648808f472cb165e63f60722394acf0
wagnerSfarias <wagner@universalhost.com.br>
2024-02-07 22:31:36 -0300

    refactor: user state

--- src/contexts/app.js
+++ src/contexts/app.js
@@ -1,9 +1,15 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const AppContext = createContext({});
 
 const ContextProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  const [user, setUser] = useState({});
+
+  return (
+    <AppContext.Provider value={{ user, setUser }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export default ContextProvider;
