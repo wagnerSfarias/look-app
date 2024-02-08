9b945d488f673fde673bf13193dd15790cf8d20d
wagnerSfarias <wagner@universalhost.com.br>
2024-02-08 18:45:05 -0300

    feat: request post and story data from api

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
+import { Box, Spacer, ScrollView } from '../../components/index';
+import api from '../../services/api';
 
 import Header from '../../components/Header';
-import { Box, Spacer, ScrollView } from '../../components/index';
-import PostList from '../../components/Post/list';
 import StoryList from '../../components/Story/list';
+import PostList from '../../components/Post/list';
+import Empty from '../../components/Empty';
 
-export default function Feed() {
+export default function Feed({ navigation }) {
+  const [loading, setLoading] = useState(false);
+  const [feed, setFeed] = useState({
+    stories: [],
+    posts: []
+  });
+
+  async function getFeed() {
+    try {
+      setLoading(true);
+      const { data: feedData } = await api.get('/feed');
+
+      setTimeout(() => {
+        setFeed(feedData);
+        setLoading(false);
+      }, 2000);
+    } catch (err) {
+      setLoading(false);
+      Alert.alert('Error ', err.message);
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getFeed();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <Box background="light">
       <Header title="Explore" />
-      <ScrollView>
-        <StoryList />
-        <Spacer />
-        <PostList />
-      </ScrollView>
+      {loading && <Empty loading />}
+      {!loading && (
+        <ScrollView>
+          <StoryList stories={feed?.stories} />
+          <Spacer />
+          <PostList posts={feed?.posts} />
+        </ScrollView>
+      )}
     </Box>
   );
 }
