70458f69af08e1a9f45ef377d4e7664d10ea4867
wagnerSfarias <wagner@universalhost.com.br>
2024-02-09 09:26:13 -0300

    feat: request data api

--- src/pages/Marketplace/category.jsx
+++ src/pages/Marketplace/category.jsx
@@ -1,23 +1,63 @@
-import React from 'react';
-import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 import Header from '../../components/Header';
 import { Touchable } from '../../components/index';
 import ProductList from '../../components/Product/list';
+import Empty from '../../components/Empty';
 
-export default function Category() {
+import api from '../../services/api';
+
+export default function Category({ route, navigation }) {
+  const { category } = route?.params;
+
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    try {
+      setLoading(true);
+      const { data: productsData } = await api.get('/products', {
+        params: {
+          categoryId: category?.id
+        }
+      });
+
+      setTimeout(() => {
+        setProducts(productsData);
+        setLoading(false);
+      }, 2000);
+    } catch (err) {
+      setLoading(false);
+      Alert.alert('Error', err.message);
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getProducts();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <>
       <Header
-        title="Category X"
+        title={category?.title}
         goBack
         right={() => (
-          <Touchable hasPadding width="70px" onPress={() => alert('teste')}>
+          <Touchable
+            hasPadding
+            width="70px"
+            onPress={() => navigation.navigate('Cart')}
+          >
             <Icon name="bag" size={20} />
           </Touchable>
         )}
       />
-      <ProductList />
+      {loading && <Empty loading />}
+      {!loading && <ProductList products={products} />}
     </>
   );
 }
--- src/pages/Marketplace/index.jsx
+++ src/pages/Marketplace/index.jsx
@@ -1,22 +1,55 @@
-import React from 'react';
-import Icon from 'react-native-vector-icons/SimpleLineIcons';
-
-import CategoryList from '../../components/Category/list';
-import Header from '../../components/Header';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import { Touchable } from '../../components/index';
+import api from '../../services/api';
 
-export default function Marketplace() {
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import CategoryList from '../../components/Category/list';
+import Empty from '../../components/Empty';
+import Header from '../../components/Header';
+
+export default function Marketplace({ navigation }) {
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  async function getCategories() {
+    try {
+      setLoading(true);
+      const { data: categoriesData } = await api.get('/categories');
+
+      setTimeout(() => {
+        setCategories(categoriesData);
+        setLoading(false);
+      }, 2000);
+    } catch (err) {
+      setLoading(false);
+      Alert.alert('Error ', err.message);
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getCategories();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <>
       <Header
         title="Categories"
         right={() => (
-          <Touchable hasPadding width="70px" onPress={() => alert('teste')}>
+          <Touchable
+            hasPadding
+            width="70px"
+            onPress={() => navigation.navigate('Cart')}
+          >
             <Icon name="bag" size={20} />
           </Touchable>
         )}
       />
-      <CategoryList />
+      {loading && <Empty loading />}
+      {!loading && <CategoryList categories={categories} />}
     </>
   );
 }
