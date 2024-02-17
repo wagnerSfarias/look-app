7ee740b98a4995c45d9dd4e52f8ac752bfb71dde
wagnerSfarias <wagner@universalhost.com.br>
2024-02-17 13:12:01 -0300

    feat: requesting order data from the api

--- src/pages/Orders/index.jsx
+++ src/pages/Orders/index.jsx
@@ -1,11 +1,49 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Alert } from 'react-native';
+
 import Header from '../../components/Header';
 import OrderList from '../../components/Order/list';
+import Empty from '../../components/Empty';
 
-export default function Orders() {
+import { AppContext } from '../../contexts/app';
+import api from '../../services/api';
+
+export default function Orders({ navigation }) {
+  const [loading, setLoading] = useState(false);
+  const [orders, setOrders] = useState([]);
+  const { user } = useContext(AppContext);
+
+  async function getOrder() {
+    try {
+      setLoading(true);
+      const { data: ordersData } = await api.get('/orders', {
+        params: {
+          userId: user?.id
+        }
+      });
+
+      setTimeout(() => {
+        setOrders(ordersData);
+        setLoading(false);
+      }, 2000);
+    } catch (err) {
+      setLoading(false);
+      Alert.alert('Error', err.message);
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getOrder();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <>
       <Header title="Orders" />
-      <OrderList />
+      {loading && <Empty loading />}
+      {!loading && <OrderList orders={orders} />}
     </>
   );
 }
